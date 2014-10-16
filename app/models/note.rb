bc9e7fd0b812ded94b7c021c9d6adc4cef6bea23
LC <eternal.kadir@gmail.com>
2014-10-16 17:37:02 +0000

    validation and better styling

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,16 @@
+require 'date'
 class Note < ActiveRecord::Base
+  include ActiveModel::Validations
+
+  validate :validate_deadline
+  validates :title, presence: true
+
+  private
+  def deadline_age
+    Date.today.mjd - self.deadline.mjd
+  end
+
+  def validate_deadline
+    errors.add(:deadline,"already passed #{deadline_age} days ago") unless deadline_age < 0
+  end
 end
